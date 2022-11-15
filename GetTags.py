6709c2fec30f794e4b9b857a39e109f13cfd87bb
Simranjeet Singh <38141273+simranjeet97@users.noreply.github.com>
2022-11-15 22:08:57 +0530

    Get Tags from the CSV downloaded by "Keyword Tool"

---
+++ GetTags.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--file', type=str, required=True)
+parser.add_argument('--p', type=str, default="/Users/simranjeetsingh1497/Downloads/")
+parser.op('--file', type=str, required=True)
+args = parser.parse_args()
+
+def gettags():
+    df = pd.read_csv(args.file)
+    data=[]
+    for i in df.values:
+        data.append(i[0])
+    result = ",".join(data)
+    return result
+
+print(gettags())
